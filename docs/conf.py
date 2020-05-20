33feac00260ba27cd7ff73d62a08224b9a5425ec
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2020-05-21 01:40:34 +0900

    Bump version to 0.1.1
    update Release.md

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../notebooks'))
 project = '深層学習チュートリアル'
 author = '人工知能画像診断学共同研究講座'
 copyright = '2020, ' + author
-version = '0.1.0'
+version = '0.1.1'
 release = version
 editors = ['Yuki Suzuki']
 
